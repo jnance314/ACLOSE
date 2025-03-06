4d6e81105c1daa9f339e9ca76548abcc555b84fd
Joe Nance <joe@nceno.app>
2025-03-06 19:10:05 +0800

    formatted init with black

--- aclose/__init__.py
+++ aclose/__init__.py
@@ -2,6 +2,6 @@ from .clustering import run_clustering
 from .labeling import add_labels
 from .plotting import silhouette_fig, scatter_fig, bars_fig
 
-__all__ = ['run_clustering', 'add_labels', 'silhouette_fig', 'scatter_fig', 'bars_fig']
+__all__ = ["run_clustering", "add_labels", "silhouette_fig", "scatter_fig", "bars_fig"]
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
